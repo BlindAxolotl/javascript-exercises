83d717782274b094946e09425ee829763909ca46
BlindAxolotl <167556385+BlindAxolotl@users.noreply.github.com>
2024-09-23 15:30:41 -0500

    Finished exercise 03.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  let len = string.length;
+  let newstr = "";
 
+  for (let i = 0; i < len; i++) {
+    newstr += string.at(len - 1 - i);
+  }
+
+  return newstr;
 };
 
 // Do not edit below this line
