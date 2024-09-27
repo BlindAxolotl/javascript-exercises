1b1917c078f0943bb01e0c2860098aa06cd7e039
BlindAxolotl <167556385+BlindAxolotl@users.noreply.github.com>
2024-09-27 10:51:02 -0500

    Finished exercise 07.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let converted = ((temp - 32) * 5) / 9;
+
+  converted = parseFloat(converted.toFixed(1));
+
+  return converted;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let converted = (temp * 9) / 5 + 32;
+
+  converted = parseFloat(converted.toFixed(1));
+
+  return converted;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
